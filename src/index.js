aa506ea0cce61aa9e926d1e2afe3b682dfec0080
ascotttoney <ascotttoney@gmail.com>
2019-06-06 09:58:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+fetch("https://randomuser.me/api/")
+  .then(res => res.json())
+  .then(json => userData(json))
+  .catch(err => console.log(err));
+
+function userData(json){
+  console.log("userData data", json);
+  // fullname.innerText = json.
+}
+
+userData;
+
+
+
+// function addNewBreed(breed){
+//   const breed_list = document.querySelector('#dog-breeds')
+//   const li = document.createElement('li')
+//   li.innerText = breed
+//   breed_list.appendChild(li)
+// }
