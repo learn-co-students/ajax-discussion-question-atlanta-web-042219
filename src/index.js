81a5f6dc895a48bbb01838ffa917301b4d1b8ffd
Preston Elliott <preston.elliott24@gmail.com>
2019-06-11 09:25:01 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-const fullname = document.getElementById("fullname");
+const fullname = document.getElementById("fullname")
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+  fetchPerson()
+  const btn = document.querySelector('button')
+  btn.addEventListener('click', fetchPerson)
+})
+
+function fetchPerson(){
+  return fetch("https://randomuser.me/api/")
+    .then(resp => resp.json())
+    .then(json => createPerson(json.results[0]))
+}
+
+function createPerson(person){
+  const img = document.querySelector("#profile_picture")
+  const name = document.querySelector("#fullname")
+  const email = document.querySelector("#email")
+  const street = document.querySelector("#street")
+  const city = document.querySelector("#city")
+  const state = document.querySelector("#state")
+  const postcode = document.querySelector("#postcode")
+  const phone = document.querySelector("#phone")
+  const cell = document.querySelector("#cell")
+  const dob = document.querySelector("#date_of_birth")
+  img.src = person.picture.thumbnail
+  name.innerText = `${person.name.title} ${person.name.first} ${person.name.last}`
+  email.innerText = person.email
+  street.innerText = person.location.street
+  city.innerText = person.location.city
+  state.innerText = person.location.state
+  postcode.innerText = person.location.postcode
+  phone.innerText = person.phone
+  cell.innerText = person.cell
+  dob.innerText = person.dob.date
+}
